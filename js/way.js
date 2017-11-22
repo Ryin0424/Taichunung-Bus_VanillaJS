d2d0d24df5dcad70a974ed97e79533c43fb85ac6
Ryin0424 <a30729522@gmail.com>
2017-11-22 12:31:30 +0800

    Fix way bug
    - 預設載入時直接渲染出 去程路線
    - 修復 去程回程 切換時發生的 bug
    - setInterval 修改 (拜託別再把 api call爆了)
    - 天啊上一次 commit 我居然把 bug 打成 buf  QQ

--- js/way.js
+++ js/way.js
@@ -8,8 +8,6 @@ var busList = document.getElementById('bus-way-list');
 goBtn.addEventListener('click', getGoJson, false);
 backBtn.addEventListener('click', getBackJson, false);
 
-// var getJSON;
-
 var helper = {
     getParameterByName: function (name, url) {
         var regex, results;
@@ -36,27 +34,42 @@ var BackUrl = `https://ptx.transportdata.tw/MOTC/v2/Bus/EstimatedTimeOfArrival/C
 
 var address = document.getElementById('now-position'); //為了渲染成路線號
 address.innerHTML = roadLine + ` 號公車路線`;
-var updata;
+var updata; //記錄要渲染的路線資料
+
+/* 判斷去程回程 */
+var check = 'go';
+
+function checkWay() {
+    if (check == "go"){
+        getGoJson();
+    }
+    else{
+        getBackJson();
+    }
+    
+}
+checkWay();
 
 /* 首次執行，先渲染出 去程回程路線選擇按鈕 */
-function choseBtn(){
-    var xhr = new XMLHttpRequest();
-    xhr.open('get', GoUrl);
-    xhr.send(null);
-    xhr.onload = function () {
-            goData = JSON.parse(xhr.responseText);
-            const len = goData.length;
-            for (var i = 0; i < len; i++) {
-                goBtn.innerHTML = `往 ${goData[len - 1].StopName.Zh_tw}`;
-                backBtn.innerHTML = `往 ${goData[0].StopName.Zh_tw}`;
-            }
-        }
-}
-choseBtn();
+// function choseBtn(){
+//     var xhr = new XMLHttpRequest();
+//     xhr.open('get', GoUrl);
+//     xhr.send(null);
+//     xhr.onload = function () {
+//             goData = JSON.parse(xhr.responseText);
+//             const len = goData.length;
+//             for (var i = 0; i < len; i++) {
+//                 goBtn.innerHTML = `往 ${goData[len - 1].StopName.Zh_tw}`;
+//                 backBtn.innerHTML = `往 ${goData[0].StopName.Zh_tw}`;
+//             }
+//         }
+// }
+// choseBtn();
 
 /* 去程資料 */
 function getGoJson(){
     clearInterval(getBackJson); // 讓畫面不會渲染出 回程路線
+    check = "go";
 
     var xhr = new XMLHttpRequest();
     xhr.open('get', GoUrl);
@@ -69,8 +82,8 @@ function getGoJson(){
             var str = '';
             const len = items.length;
             for (var i = 0; i < len; i++) {
-                // goBtn.innerHTML = `往 ${items[len - 1].StopName.Zh_tw}`;
-                // backBtn.innerHTML = `往 ${items[0].StopName.Zh_tw}`;
+                goBtn.innerHTML = `往 ${items[len - 1].StopName.Zh_tw}`;
+                backBtn.innerHTML = `往 ${items[0].StopName.Zh_tw}`;
 
                 const Time = Math.floor(items[i].EstimateTime / 60); //將到站時間換算成分鐘
                 if (items[i].EstimateTime == undefined) { //暫無公車靠近，顯示 過站
@@ -127,16 +140,14 @@ function getGoJson(){
         }
         update(goData);
     }
-    // getJSON = getGoJson;
-    // setInterval(getJSON, 30000);
-    setInterval(getGoJson, 30000);
+    // setInterval(getGoJson, 30000);
 }
-// getGoJson();
 
 
 /* 回程資料 */
 function getBackJson() {
     clearInterval(getGoJson); // 讓畫面不會渲染出 去程路線
+    check = "back";
     
     var xhr = new XMLHttpRequest();
     xhr.open('get', BackUrl);
@@ -203,9 +214,8 @@ function getBackJson() {
         }
         update(backData);
     }
-    // getJSON = getBackJson;
-    // setInterval(getJSON, 30000);
-    setInterval(getBackJson, 30000);
+    // setInterval(getBackJson, 30000);
 }
 
+setInterval(checkWay, 30000);
 
