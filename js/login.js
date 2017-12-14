f469b2c0316b4716613c9dce9d2e0cb2118223bd
Ryin0424 <a30729522@gmail.com>
2017-12-14 19:44:23 +0800

    Fixed login search
    - 搜尋後產生的 list 可加入最愛
    - 英文版本功能未完成
    
    重大 bug
    - 重複登入、登出會導致資料重覆推送

--- js/login.js
+++ js/login.js
@@ -1,8 +1,8 @@
 const Fblogin = document.getElementById('Fblogin');
 
-function toggleSignIn () {
+function toggleSignIn() {
     event.preventDefault();
-    
+
     // 登入開始
     if (!firebase.auth().currentUser) {
         const provider = new firebase.auth.FacebookAuthProvider();
@@ -34,16 +34,18 @@ function initApp() {
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
             // 若使用者已登入
+            
             let name = user.displayName; // 記錄使用者信名資料
             var email = user.email;
             var isAnonymous = user.isAnonymous;
-            var uid = user.uid;
+            // var uid = user.uid;
+            uid = user.uid;
             var providerData = user.providerData;
-            
+
             var userData = firebase.database().ref(uid); // 指向(建立)一個 uid變數 為名的 object
             var userArray = []; // 建立一個空陣列以儲存 所有 user 的 uid
             // console.log(userData);
-            console.log(`這是登入時使用者的uid： `+uid);
+            console.log(`這是登入時使用者的uid： ` + uid);
 
             firebase.database().ref().once('value', function (snapshot) {
                 snapshot.forEach(function (items) {
@@ -53,10 +55,10 @@ function initApp() {
                 // console.log(userArray);
                 // console.log(userArray.join(` `));
                 // console.log(userArray.join(` `).indexOf(uid) > -1);
-                if (userArray.indexOf(uid) > -1 == true){ // 若已儲存該筆 uid 資料
+                if (userArray.indexOf(uid) > -1 == true) { // 若已儲存該筆 uid 資料
                     console.log(`已存在該筆 uid 資料`);
                     // 這邊要把 Firebase.database list、favorite 的資料 innerHTML
-                }else{
+                } else {
                     console.log(`推送資料`);
                     for (i in data) {
                         userData.child("list").push({ // 推送資料到 Firebase database
@@ -70,57 +72,96 @@ function initApp() {
                 }
             })
             document.getElementById('now-position').innerHTML = `歡迎，${name}`;
-            Fblogin.innerHTML = 
-            `<div class="fa fa-facebook-official" style="color: red"></div>
+            Fblogin.innerHTML =
+                `<div class="fa fa-facebook-official" style="color: red"></div>
             <span>Facebook登出</span>`;
 
             /* 登入之後渲染路線(Favorite、list) */
             function showUserList() {
-                function showFavorite() {
-                    userData.child("favorite").orderByChild('route_ZH').on('value', function (snapshot) {
-                        // var data = snapshot.val();
-                        // console.log(data);
-                        var str= "";
-                        snapshot.forEach(function (i) {
-                            var data = i.val();
-                            str +=
-                            `<li class="card" title="路線詳情">
-                                <i class="fa fa-thumb-tack" title="取消最愛路線" data-key="${i.key}" data-route_zh="${data.route_ZH}" data-route_en="${data.route_EN}" data-way_zh="${data.way_ZH}" data-way_en_first="${data.way_EN_First}" data-way_en_end="${data.way_EN_End}"></i>
-                                <a href="bus-way.html?Zh_tw=${data.route_ZH}&En=${data.route_EN}" class="busLink">
-                                    <p class="bus-way">${data.way_ZH}</p>
-                                    <p class="bus-num">${data.route_ZH}</p>
-                                 </a>
-                            </li>`
-                        })
-                        favorite.innerHTML = str;
+                userData.child("favorite").orderByChild('route_ZH').on('value', function (snapshot) {
+                    // var data = snapshot.val();
+                    // console.log(data);
+                    var str = "";
+                    snapshot.forEach(function (i) {
+                        var data = i.val();
+                        str +=
+                        `<li class="card" title="路線詳情">
+                            <i class="fa fa-thumb-tack" title="取消最愛路線" data-key="${i.key}" data-route_zh="${data.route_ZH}" data-route_en="${data.route_EN}" data-way_zh="${data.way_ZH}" data-way_en_first="${data.way_EN_First}" data-way_en_end="${data.way_EN_End}"></i>
+                            <a href="bus-way.html?Zh_tw=${data.route_ZH}&En=${data.route_EN}" class="busLink">
+                                <p class="bus-way">${data.way_ZH}</p>
+                                <p class="bus-num">${data.route_ZH}</p>
+                            </a>
+                        </li>`
                     })
-                }
-                function showList() {
-                    userData.child("list").orderByChild('route_ZH').on('value',function(snapshot){
-                        var str ='';
-                        snapshot.forEach(function (i) {
-                            var data = i.val();
-                            str +=
-                                `<li class="card" title="路線詳情">
-                                <i class="fa fa-thumb-tack" title="加到最愛路線" data-key="${i.key}" data-route_zh="${data.route_ZH}" data-route_en="${data.route_EN}" data-way_zh="${data.way_ZH}" data-way_en_first="${data.way_EN_First}" data-way_en_end="${data.way_EN_End}"></i>
-                                <a href="bus-way.html?Zh_tw=${data.route_ZH}&En=${data.route_EN}" class="busLink">
-                                    <p class="bus-way">${data.way_ZH}</p>
-                                    <p class="bus-num">${data.route_ZH}</p>
-                                 </a>
-                            </li>`
+                    favorite.innerHTML = str;
+                })
+
+                userData.child("list").orderByChild('route_ZH').on('value', function (snapshot) {
+                    var str = '';
+                    snapshot.forEach(function (i) {
+                        var data = i.val();
+                        str +=
+                        `<li class="card" title="路線詳情">
+                            <i class="fa fa-thumb-tack" title="加到最愛路線" data-key="${i.key}" data-route_zh="${data.route_ZH}" data-route_en="${data.route_EN}" data-way_zh="${data.way_ZH}" data-way_en_first="${data.way_EN_First}" data-way_en_end="${data.way_EN_End}"></i>
+                            <a href="bus-way.html?Zh_tw=${data.route_ZH}&En=${data.route_EN}" class="busLink">
+                                <p class="bus-way">${data.way_ZH}</p>
+                                <p class="bus-num">${data.route_ZH}</p>
+                            </a>
+                        </li>`
+                    })
+                    list.innerHTML = str;
+
+                    /* 資料庫端的搜尋功能 */
+                    search.addEventListener('input', searchBusDatabase, false);
+                    function searchBusDatabase(){
+                        var searchVu = search.value;
+                        var databaseAry = [];
+
+                        snapshot.forEach(function (items) {
+                            var dataBaseList = {
+                                "key": items.key,
+                                "route_ZH": items.val().route_ZH,
+                                "route_EN": items.val().route_EN,
+                                "way_ZH": items.val().way_ZH,
+                                "way_EN_Firtst": items.val().way_EN_Firtst,
+                                "way_EN_End": items.val().way_EN_End,
+                            }
+                            databaseAry.push(dataBaseList);
                         })
-                        list.innerHTML = str;
-                    })  
-                }
-                showFavorite();
-                showList();
+                        // console.log(databaseAry);
+
+                        function filterItems(searchVu) {
+                            return databaseAry.filter(function (i) {
+                                return i.route_ZH.indexOf(searchVu.toUpperCase()) == 0;
+                            })
+                        }
+                        // console.log(filterItems(searchVu));
+
+                        function updatedList(items) { // 重新渲染路線結果
+                            var str = '';
+                            for (var i = 0; i < items.length; i++) {
+                                // console.log(items[i].key);
+                                str +=
+                                `<li class="card" title="路線詳情">
+                                    <i class="fa fa-thumb-tack" title="加到最愛路線" data-key="${items[i].key}" data-route_zh="${items[i].route_ZH}" data-route_en="${items[i].route_EN}" data-way_zh="${items[i].way_ZH}" data-way_en_first="${items[i].way_EN_First}" data-way_en_end="${items[i].way_EN_End}"></i>
+                                    <a href="bus-way.html?Zh_tw=${items[i].route_ZH}&En=${items[i].route_EN}" class="busLink">
+                                        <p class="bus-way">${items[i].way_ZH}</p>
+                                        <p class="bus-num">${items[i].route_ZH}</p>
+                                    </a>
+                                </li>`    
+                            }
+                            list.innerHTML = str;
+                        }
+                        updatedList(filterItems(searchVu));
+                    }
+                })
             }
             showUserList();
+
             /* 增加、刪除最愛路線 */
-            function toggleFavorite() {
-                console.log(`brabra：` + uid);
+            // function toggleFavorite() {
                 list.addEventListener('click', function (e) {
-                    if (e.target.nodeName = "I"){
+                    if (e.target.nodeName = "I") {
                         var key = e.target.dataset.key; // dataset 讀取 data 中的項目，這邊讀取的我們設定的 data-key
                         var route_zh = e.target.dataset.route_zh;
                         var route_en = e.target.dataset.route_en;
@@ -161,20 +202,23 @@ function initApp() {
                         console.log(`Favorite：成功刪除 #favorite 資料！`);
                     }
                 })
-            }
-            toggleFavorite();
+            // }
+            // toggleFavorite();
 
         } else { /* 登出時候的狀態 */
             // console.log(userData);
             console.log(`這是登出時使用者的uid： ` + uid);
             document.getElementById('now-position').innerHTML = `　`;
-            Fblogin.innerHTML = 
-            `<div class="fa fa-facebook-official"></div>
+            Fblogin.innerHTML =
+                `<div class="fa fa-facebook-official"></div>
             <span>Facebook登入</span>`;
             list.innerHTML = OriginalList;
             favorite.innerHTML = '';
+            
+            
+
+            search.addEventListener('input', searchBusLocal, false); //input屬性 = 使用者每次操作都跑一次 function
         }
     });
     Fblogin.addEventListener('click', toggleSignIn, false);
 }
-
